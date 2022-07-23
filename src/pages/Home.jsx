cba9a58bc8418093f17742438fdafba34bf4f03b
Muhammed Ali SUSLU <malisuslu@yandex.com>
2022-07-23 16:19:42 +0300

    receipe app id corrected

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
   const fetchRecipe = async (e) => {
     e.preventDefault();
     const response = await axios.get(
-      `https://api.edamam.com/api/recipes/v2?type=public&q=${recipe}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&mealType=${meal}`
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${recipe}&app_id=${process.env.REACT_APP_APL_ID}&app_key=${process.env.REACT_APP_APP_KEY}&mealType=${meal}`
     );
     setRecipes(response.data.hits);
     setHeader(`${recipe} on ${meal}`);
